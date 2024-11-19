a37661e101b3703f57162e03d6eee6e6602208ce
Nyken000 <javier.manriquez10@inacapmail.cl>
2024-11-19 18:09:41 -0300

    V0.1

--- usuariosApp/forms.py
+++ usuariosApp/forms.py
@@ -12,7 +12,7 @@ class RegistroForm(forms.ModelForm):
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data['password'])  # Encriptar la contraseña
-        user.rol = 'usuario'  # Asignar el rol "usuario" por defecto
+        user.rol = 'admin'  # Asignar el rol "usuario" por defecto
         if commit:
             user.save()
         return user
